76e365c0bc982d553b788ec1a1b59a60d2da249d
mehrad <s.mehrad.hosseini@gmail.com>
2022-06-13 11:17:29 +0430

    animation for task moving added

--- scripts/app/card/task/move_task.js
+++ scripts/app/card/task/move_task.js
@@ -4,21 +4,31 @@ const move_task = (usename_of_user,card_id,task_id,task_value) =>{
     const new_card_id = cards[user_card_task_index[0]].card[user_card_task_index[1]+1].id
 
 
-    if(new_card_id){
+        document.getElementById(task_id).style.opacity="0"
 
-        cards[user_card_task_index[0]].card[user_card_task_index[1]].tasks.splice(user_card_task_index[2],1)
+        setTimeout(()=>{
+            if(new_card_id){
+
+                cards[user_card_task_index[0]].card[user_card_task_index[1]].tasks.splice(user_card_task_index[2],1)
+                
+                const num_new_task_id = create_id_task(usename_of_user,new_card_id)
+                const new_task_id = `${new_card_id}-task${num_new_task_id}`
+                
+                cards[user_card_task_index[0]].card[user_card_task_index[1]+1].tasks.push({"id" : new_task_id , "value" : task_value} )
+                
+                task_list_when_add_card(usename_of_user)
+
         
-        const num_new_task_id = create_id_task(usename_of_user,new_card_id)
-        const new_task_id = `${new_card_id}-task${num_new_task_id}`
-        
-        cards[user_card_task_index[0]].card[user_card_task_index[1]+1].tasks.push({"id" : new_task_id , "value" : task_value} )
-        
-        task_list_when_add_card(usename_of_user)
+            }else{
+                alert("error")
+            }
+        }, 300)
 
 
-    }else{
-        alert("error")
-    }
+
+
+    
+
     document.getElementById("close-edit-box-task" ).style.display ="none";
 
 
