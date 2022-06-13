e731381f8000815bbf6f5843e7e26a7827da89dc
mehrad <s.mehrad.hosseini@gmail.com>
2022-06-13 11:44:56 +0430

    animation for task deleting added

--- scripts/app/card/task/delete_task.js
+++ scripts/app/card/task/delete_task.js
@@ -1,10 +1,13 @@
 const del_task = (usename_of_user,card_id,task_id) =>{
 
     const task_index =  find_taskid_in_cards(usename_of_user,card_id,task_id)
-    cards[task_index[0]].card[task_index[1]].tasks.splice(task_index[2],1)
+    
+    document.getElementById(task_id).style.opacity="0"
+    setTimeout(()=>{
 
-
-    task_list_when_add_card(usename_of_user)
+        cards[task_index[0]].card[task_index[1]].tasks.splice(task_index[2],1)
+        task_list_when_add_card(usename_of_user)
+    }, 300)
     document.getElementById("close-edit-box-task" ).style.display ="none";
 
 
